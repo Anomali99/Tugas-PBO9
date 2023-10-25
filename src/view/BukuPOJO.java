8ddc0a5c820d9ca58d703f4362c3cc78ffd779e6
Anomali99 <fatiqnur@gmail.com>
2023-10-25 20:03:10 +0700

    first commit

---
+++ src/view/BukuPOJO.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author fatiq
+ */
+public class BukuPOJO {
+    private String isbn;
+    private String judul;
+    private String tahun;
+    private String penerbit;
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the judul
+     */
+    public String getJudul() {
+        return judul;
+    }
+
+    /**
+     * @param judul the judul to set
+     */
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    /**
+     * @return the tahun
+     */
+    public String getTahun() {
+        return tahun;
+    }
+
+    /**
+     * @param tahun the tahun to set
+     */
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+
+    /**
+     * @return the penerbit
+     */
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    /**
+     * @param penerbit the penerbit to set
+     */
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+}
